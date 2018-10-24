48592fac57201c9c1eaf9df932077861ecc40b60
Bruno Beloff <bruno.beloff@southcoastscience.com>
2018-10-24 11:55:19 +0100

    Updated git_pull.sh

--- git_pull.sh
+++ git_pull.sh
@@ -1,23 +1,32 @@
 #!/usr/bin/env bash
 
-# An example git pull script, normally copied to ~/SCS and incorporating all repos
-
-GIT_PATH=~/SCS/scs_ndir/
-echo ${GIT_PATH}
-git -C ${GIT_PATH} pull
-echo '-'
-
 GIT_PATH=~/SCS/scs_core/
 echo ${GIT_PATH}
 git -C ${GIT_PATH} pull
 echo '-'
 
-GIT_PATH=~/SCS/scs_host_posix/          # replace with the appropriate host package
+GIT_PATH=~/SCS/scs_dev/
 echo ${GIT_PATH}
 git -C ${GIT_PATH} pull
 echo '-'
 
-GIT_PATH=~/SCS/scs_osio/
+#GIT_PATH=~/SCS/scs_dfe_eng/        # replace with the appropriate board package, as necessary
+#echo ${GIT_PATH}
+#git -C ${GIT_PATH} pull
+#echo '-'
+
+GIT_PATH=~/SCS/scs_host_rpi/        # replace with the appropriate host package, as necessary
 echo ${GIT_PATH}
 git -C ${GIT_PATH} pull
 echo '-'
+
+GIT_PATH=~/SCS/scs_mfr/
+echo ${GIT_PATH}
+git -C ${GIT_PATH} pull
+echo '-'
+
+GIT_PATH=~/SCS/scs_ndir/           # uncomment if the NDIR package is installed
+echo ${GIT_PATH}
+git -C ${GIT_PATH} pull
+echo '-'
+
