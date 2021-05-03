24850221b0fd18fe1ca267bf644077246ccb4a7e
Bruno Beloff <bruno.beloff@southcoastscience.com>
2021-05-03 13:51:30 +0100

    Refactored setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 """
 Created on 4 Sep 2020
 Updated 23 Mar 2021
@@ -38,7 +40,7 @@ with open('requirements.txt') as req_txt:
 
 setup(
     name='scs_ndir',
-    version=get_version("scs_ndir/__init__.py"),
+    version=get_version("src/scs_ndir/__init__.py"),
     description='Environmental sampling abstractions for the South Coast Science NDIR SPI board.',
     author='South Coast Science',
     author_email='contact@southcoastscience.com',
